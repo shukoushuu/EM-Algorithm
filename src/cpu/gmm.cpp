be11d27c34eb93ce76c90e79f7b55f73fb882d83
ranger <github_mail@protonmail.com>
2017-02-24 10:48:59 +0800

    Add some comments to the CPU version

--- src/cpu/gmm.cpp
+++ src/cpu/gmm.cpp
@@ -15,26 +15,26 @@ GMM::GMM(size_t numGaussianas, double* pesos, double* medias, double* covarianza
          unsigned int maxIteraciones = 250, double precision = 1e-5)
 {
     if (pesos == NULL || medias == NULL || covarianzas == NULL) {
-        cerr << "ERROR: puntero NULL en la inicializacion.\n";
+        cerr << "ERROR: puntero NULL en la inicializacion.\n"; // 初始化使用了空指针
         throw 1;
     }
 
     if (numGaussianas < 1) {
-        cerr << "ERROR: debe haber al menos 1 gaussiana.\n";
+        cerr << "ERROR: debe haber al menos 1 gaussiana.\n"; // 至少要有1个高斯
         throw 1;
     }
 
-    this->numGaussianas = numGaussianas;
+    this->numGaussianas = numGaussianas; // 高斯mixture数量
 
-    this->pesos = pesos;
-    this->medias = medias;
-    this->covarianzas = covarianzas;
+    this->pesos = pesos; // peso=weight 各mixture权重
+    this->medias = medias; // 各mixture均值
+    this->covarianzas = covarianzas; // 各mixture协方差矩阵
 
-    this->datos = NULL;
-    this->maxIteraciones = maxIteraciones;
-    this->precision = precision;
+    this->datos = NULL; // 输入的数据
+    this->maxIteraciones = maxIteraciones; // 最大迭代次数
+    this->precision = precision; // 停止精度
 
-    this->verosimilitud = MENOS_INFINITO;
+    this->verosimilitud = MENOS_INFINITO; // 似然初始化为负无穷
 }
 
 GMM::~GMM()
@@ -49,16 +49,16 @@ GMM::~GMM()
     delete [] this->v;
 }
 
-resultado_type GMM::estimar(double *datos, size_t numMuestras, size_t numDimensiones)
+resultado_type GMM::estimar(double *datos, size_t numMuestras, size_t numDimensiones) // estimar = estimate
 {
     if (datos == NULL || numMuestras < 1 || numDimensiones < 1) {
-        cerr << "ERROR: no se han cargado los datos.\n";
+        cerr << "ERROR: no se han cargado los datos.\n"; // Data not uploaded未载入数据
         throw 1;
     }
 
     unsigned int i;
-    bool finalizado = false;
-    double ultimaVerosimilitud = verosimilitud;
+    bool finalizado = false; // finalizado=finalized 迭代是否停止
+    double ultimaVerosimilitud = verosimilitud; // 最后一次迭代的似然
 
     this->resp = new double[numMuestras * numGaussianas];
     this->L = new double[numDimensiones * numDimensiones];
@@ -69,13 +69,13 @@ resultado_type GMM::estimar(double *datos, size_t numMuestras, size_t numDimensi
     this->numDimensiones = numDimensiones;
 
     for (i = 1; i <= maxIteraciones; i++) {
-        if (i % 10 == 1) {
+        if (i % 10 == 1) { // 每10次循环输出一次
             cerr << ".";
         }
 
-        calcular();
+        calcular(); // 迭代中执行具体计算的函数
 
-        // Test de convergencia
+        // Test de convergencia // 测试是否收敛: 本次迭代中似然的相对变化是否小于停止精度
         if (abs((verosimilitud - ultimaVerosimilitud) / verosimilitud) <= precision) {
             finalizado = true;
 
@@ -88,7 +88,7 @@ resultado_type GMM::estimar(double *datos, size_t numMuestras, size_t numDimensi
     // Asegurar que guardamos el numero de iteracion real
     if (!finalizado) i--;
 
-    // Imprimir ultima iteracion
+    // Imprimir ultima iteracion // imprimir=print输出最后一次迭代
     imprimir(finalizado, i);
 
     resultado_type resultados;
@@ -99,23 +99,23 @@ resultado_type GMM::estimar(double *datos, size_t numMuestras, size_t numDimensi
     return resultados;
 }
 
-void GMM::calcular()
+void GMM::calcular() // calcular=calculate
 {
     double *logDets = new double[numGaussianas];
-    double *u = new double[numDimensiones];
+    double *u = new double[numDimensiones]; // 暂存(x_i - mu_k)
     double suma, verosimilitudParcial, maxContribucion, sumaProbabilidades;
 
-    // Paso E
+    // Paso E // E step 求log[N(x_n|mu_k, Sigma_k) * P(k)] (不包括d/2log(2*pi)那一项)
     for (size_t k = 0; k < numGaussianas; k++) {
-        cholesky(k);
-        logDets[k] = logaritmoDeterminante();
+        cholesky(k); // cholesky分解, 将对称正定的矩阵分解为一个下三角矩阵L及其转置的乘积. 这里分解的是协方差矩阵
+        logDets[k] = logaritmoDeterminante(); // logarithmDeterminant 协方差矩阵Sigma行列式对数
 
-        for (size_t i = 0; i < numMuestras; i++) {
+        for (size_t i = 0; i < numMuestras; i++) { // 计算公式3.7: log [N(x|mu, sigma) * P(k)]
             for (size_t j = 0; j < numDimensiones; j++) {
-                u[j] = datos[i * numDimensiones + j] - medias[k * numDimensiones + j];
+                u[j] = datos[i * numDimensiones + j] - medias[k * numDimensiones + j]; // 计算(x_i - mu_k)
             }
 
-            resolver(u);
+            resolver(u); // 求v=L^(-1)(x_i - mu_k)
 
             suma = 0.0;
 
@@ -123,14 +123,14 @@ void GMM::calcular()
                 suma += v[j] * v[j];
             }
 
-            resp[i * numGaussianas + k] = -0.5 * (suma + logDets[k]) + gsl_sf_log(pesos[k]);
+            resp[i * numGaussianas + k] = -0.5 * (suma + logDets[k]) + gsl_sf_log(pesos[k]); // log[N(x_n|mu_k, Sigma_k) * P(k)] (不包括d/2log(2*pi)那一项)
         }
-    }
+    } // E step结束
 
-    verosimilitud = 0.0;
+    verosimilitud = 0.0; // verosimilitud=likelihood, 似然
 
-    for (size_t i = 0; i < numMuestras; i++) {
-        maxContribucion = MENOS_INFINITO;
+    for (size_t i = 0; i < numMuestras; i++) { // numMuestras待分类的样本数量, 对每一个点n, 找到使其p_nk最大的那个mixture
+        maxContribucion = MENOS_INFINITO; // 对于给定的n, 找到使p_nk最大的k, 先初始化为无穷小
 
         for (size_t k = 0; k < numGaussianas; k++) {
             if (resp[i * numGaussianas + k] > maxContribucion) {
@@ -141,36 +141,36 @@ void GMM::calcular()
         suma = 0.0;
 
         for (size_t k = 0; k < numGaussianas; k++) {
-            suma += exp(resp[i * numGaussianas + k] - maxContribucion);
+            suma += exp(resp[i * numGaussianas + k] - maxContribucion); // resp[i * numGaussianas + k]也就是log[N(x_n|mu_k, Sigma_k) * P(k)] (不包括d/2log(2*pi)那一项), 对应下面的z_k
         }
 
-        verosimilitudParcial = maxContribucion + gsl_sf_log(suma);
+        verosimilitudParcial = maxContribucion + gsl_sf_log(suma); // 根据log-sum-exp公式 log(Sigma(exp(z_k))) = z_max + log(Sigma(exp(z_k - z_max))) verosimilitudParcial = log{Sigma[N(x_n|mu_k, Sigma_k) * P(k)]} (不包括d/2log(2*pi)那一项)
 
         for (size_t k = 0; k < numGaussianas; k++) {
-            resp[i * numGaussianas + k] = exp(resp[i * numGaussianas + k] - verosimilitudParcial);
+            resp[i * numGaussianas + k] = exp(resp[i * numGaussianas + k] - verosimilitudParcial); // resp[i * numGaussianas + k]重新赋值为[N(x_n|mu_k, Sigma_k) * P(k)] /Sigma[N(x_n|mu_k, Sigma_k) * P(k)], 这时才是真正的p_nk
         }
 
-        verosimilitud += verosimilitudParcial;
+        verosimilitud += verosimilitudParcial; // 最终求得log(Likelihood)
     }
 
-    // Paso M
-    for (size_t k = 0; k < numGaussianas; k++) {
+    // Paso M // M step
+    for (size_t k = 0; k < numGaussianas; k++) { // 对于每个mixture
         sumaProbabilidades = 0.0;
 
         for (size_t i = 0; i < numMuestras; i++) {
-            sumaProbabilidades += resp[i * numGaussianas + k];
+            sumaProbabilidades += resp[i * numGaussianas + k]; // 给定k, 计算所有p_nk之和, 也就是所有采样属于第k个mixture的概率之和
         }
 
-        pesos[k] = sumaProbabilidades / numMuestras;
+        pesos[k] = sumaProbabilidades / numMuestras; // 公式3.6 对各mixture的权重进行更新
 
         for (size_t j = 0; j < numDimensiones; j++) {
             suma = 0.0;
 
             for (size_t i = 0; i < numMuestras; i++) {
-                suma += resp[i * numGaussianas + k] * datos[i * numDimensiones + j];
+                suma += resp[i * numGaussianas + k] * datos[i * numDimensiones + j]; // Sigma(p_nk * x_n)
             }
 
-            medias[k * numDimensiones + j] = suma / sumaProbabilidades;
+            medias[k * numDimensiones + j] = suma / sumaProbabilidades; // 更新均值
 
             for (size_t h = 0; h < numDimensiones; h++) {
                 suma = 0.0;
@@ -180,26 +180,26 @@ void GMM::calcular()
                         * (datos[i * numDimensiones + h] - medias[k * numDimensiones + h]);
                 }
 
-                covarianzas[k * numDimensiones * numDimensiones + j * numDimensiones + h] = suma / sumaProbabilidades;
+                covarianzas[k * numDimensiones * numDimensiones + j * numDimensiones + h] = suma / sumaProbabilidades; // 更新协方差矩阵
             }
         }
-    }
+    } // M step结束
 
     // Limpiamos la memoria
     delete [] logDets;
     delete [] u;
 }
 
-void GMM::cholesky(size_t gaussiana)
+void GMM::cholesky(size_t gaussiana) // cholesky分解, 将对称正定的矩阵分解为一个下三角矩阵L及其转置的乘积. 这里分解的是协方差矩阵
 {
-    for (size_t i = 0; i < numDimensiones; i++) {
+    for (size_t i = 0; i < numDimensiones; i++) { // 先初始化为0
         for (size_t j = 0; j < numDimensiones; j++) {
             L[i * numDimensiones + j] = 0.0;
         }
     }
 
     for (size_t i = 0; i < numDimensiones; i++) {
-        for (size_t j = 0; j < i + 1; j++) {
+        for (size_t j = 0; j < i + 1; j++) { // 只处理下三角的元素(包括对角线上的元素)
             double suma = 0;
 
             for (size_t k = 0; k < j; k++) {
@@ -213,15 +213,15 @@ void GMM::cholesky(size_t gaussiana)
     }
 }
 
-void GMM::resolver(double *muestra)
+void GMM::resolver(double *muestra) // 求v=L^(-1)(x_i - mu_k)
 {
     double suma;
 
-    for (size_t i = 0; i < numDimensiones; i++) {
+    for (size_t i = 0; i < numDimensiones; i++) { // 先将v初始化为0
         v[i] = 0.0;
     }
 
-    for (size_t i = 0; i < numDimensiones; i++) {
+    for (size_t i = 0; i < numDimensiones; i++) { //L*y = x - mu， 求解y
         suma = muestra[i];
 
         for (size_t j = 0; j < i; j++) {
@@ -232,20 +232,20 @@ void GMM::resolver(double *muestra)
     }
 }
 
-double GMM::logaritmoDeterminante()
+double GMM::logaritmoDeterminante() // logarithmDeterminant 协方差矩阵Sigma行列式对数
 {
     double suma = 0.0;
 
     for (size_t i = 0; i < numDimensiones; i++) {
-        suma += gsl_sf_log(L[i * numDimensiones + i]);
+        suma += gsl_sf_log(L[i * numDimensiones + i]); // Cholesky分解协方差矩阵Sigma得到的L矩阵对角线上元素的对数求和, 因为行列式等于对角线元素之积
     }
 
-    return 2.0 * suma;
+    return 2.0 * suma; // Sigma的行列式等于L行列式的平方
 }
 
-void GMM::imprimir(bool finalizado, unsigned int iteracion)
+void GMM::imprimir(bool finalizado, unsigned int iteracion) // imprimir=print输出
 {
-    cerr << setprecision(5) << scientific;
+    cerr << setprecision(5) << scientific; // 指数形式输出
 
     cerr << "\n\n";
 
--- src/cpu/gmm.h
+++ src/cpu/gmm.h
@@ -6,11 +6,11 @@
 using namespace std;
 
 // Constantes
-const double MENOS_INFINITO = -numeric_limits<double>::max();
+const double MENOS_INFINITO = -numeric_limits<double>::max(); // MENOS=minimum 负无穷
 
-struct resultado_type {
-  size_t iteracion;
-  double verosimilitud;
+struct resultado_type { // resultado = result
+  size_t iteracion; // iteracion = iteration
+  double verosimilitud; // verosimilitud=likelihood 似然
 };
 
 class GMM
@@ -20,35 +20,35 @@ class GMM
     GMM(size_t numGaussianas, double* pesos, double* medias, double* covarianzas, unsigned int maxIteraciones, double precision);
     ~GMM();
 
-    resultado_type estimar(double *datos, size_t numMuestras, size_t numDimensiones);
+    resultado_type estimar(double *datos, size_t numMuestras, size_t numDimensiones); // estimar=estimate
 
   private:
 
-    double *pesos;
-    double *medias;
-    double *covarianzas;
+    double *pesos; // peso=weight 各mixture权重
+    double *medias; // 各mixture均值
+    double *covarianzas; // 各mixture协方差矩阵
 
-    double *resp;
-    double *L;
+    double *resp; // responsibility矩阵, 存储p_nk, 也就是p(k|n), 第n个点属于第k个mixture的概率
+    double *L; // cholesky分解协方差矩阵获得的下三角矩阵L L*L'为协方差矩阵
     double *v;
 
-    size_t numMuestras;
-    size_t numGaussianas;
-    size_t numDimensiones;
-    double *datos;
+    size_t numMuestras; // 待分类的样本数量
+    size_t numGaussianas; // 高斯mixture数量
+    size_t numDimensiones; // 维数
+    double *datos; // 输入的数据
 
-    unsigned int maxIteraciones;
-    double precision;
+    unsigned int maxIteraciones; // 最大迭代次数
+    double precision; // 停止精度
 
-    double verosimilitud;
+    double verosimilitud; // 似然
 
-    void calcular();
+    void calcular(); // calcular=calculate, 
 
     void cholesky(size_t gaussiana);
     void resolver(double *vector);
     double logaritmoDeterminante();
 
-    void imprimir(bool finalizado, unsigned int iteracion);
+    void imprimir(bool finalizado, unsigned int iteracion); // imprimir=print输出
 };
 
 #endif
